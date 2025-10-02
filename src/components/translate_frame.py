95938c7f0ee35e6ed0b6450223dd092561c0bfba
lukas_christopher <lukas.santana@aluno.uepb.edu.br>
2025-10-01 21:59:30 -0300

    criação do translate frame

---
+++ src/components/translate_frame.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+class TranslateFrame(Frame):
+    def __init__(self, root, command, **kwargs):
+        super().__init__(root, **kwargs)
+
+        Label(root, text="Translate").pack(side=TOP)
+
+        frame = Frame(root, height=100)
+        frame.pack(side=TOP, fill=BOTH, padx=0, pady=0)
+
+        Label(frame, text="X").grid(row=1, column=0, sticky="e", padx=5, pady=2)
+        self.x_input = Entry(frame, width=10)
+        self.x_input.grid(row=1, column=1, padx=5, pady=2)
+
+        Label(frame, text="Y").grid(row=2, column=0, sticky="e", padx=5, pady=2)
+        self.y_input = Entry(frame, width=10)
+        self.y_input.grid(row=2, column=1, padx=5, pady=2)
+
+        Label(frame, text="Z").grid(row=3, column=0, sticky="e", padx=5, pady=2)
+        self.z_input = Entry(frame, width=10)
+        self.z_input.grid(row=3, column=1, padx=5, pady=2)
+
+        btn_translate = Button(root, text="Translate", command=command)
+        btn_translate.pack(side=TOP, padx=0, pady=0)
+
+    def get_input(self):
+        x = float(self.x_input.get())
+        y = float(self.y_input.get())
+        z = float(self.z_input.get())
+
+        return [x, y, z]
+
+
--- src/edite_shape.py
+++ src/edite_shape.py
@@ -2,53 +2,42 @@ from tkinter import *
 
 from src.utils.windowtk import WindowTk
 
+from src.components.translate_frame import TranslateFrame
+
 class EditShape(Frame):
     def __init__(self, root, gl_window: WindowTk, **kwargs):
         super().__init__(root, **kwargs)
         self.gl_window = gl_window
-    
+
         frame = Frame(root, height=100)
-        frame.pack(side=BOTTOM, fill=BOTH, padx=0, pady=0)
+        frame.pack(side=TOP, fill=BOTH, padx=0, pady=0)
 
-        #Translate
-        Label(frame, text="Translate").grid(row=0, column=0, columnspan=2, pady=5)
-
-        Label(frame, text="X").grid(row=1, column=0, sticky="e", padx=5, pady=2)
-        self.x_input = Entry(frame, width=10)
-        self.x_input.grid(row=1, column=1, padx=5, pady=2)
-
-        Label(frame, text="Y").grid(row=2, column=0, sticky="e", padx=5, pady=2)
-        self.y_input = Entry(frame, width=10)
-        self.y_input.grid(row=2, column=1, padx=5, pady=2)
-
-        Label(frame, text="Z").grid(row=3, column=0, sticky="e", padx=5, pady=2)
-        self.z_input = Entry(frame, width=10)
-        self.z_input.grid(row=3, column=1, padx=5, pady=2)
-
-        btn_translate = Button(frame, text="Translate", command=self.translate)
-        btn_translate.grid(row=4, column=0, columnspan=2, pady=10)
+        self.translate_frame = TranslateFrame(frame, command=self.translate)
 
         #Rotation
-        Label(frame, text="Angulo:").grid(row=5, column=0, sticky="e", padx=5, pady=2)
+        # Label(frame, text="Angulo").pack(side=LEFT)
+        # rotation_frame = Frame(frame, height=100)
+        # frame.pack(side=TOP, fill=BOTH, padx=0, pady=0)
 
-        self.rotation_input = Entry(frame, width=10)
-        self.rotation_input.grid(row=5, column=1, padx=5, pady=2)
 
-        btn_rotation = Button(frame, text="Rotation", command=self.rotation)
-        btn_rotation.grid(row=5, column=2, padx=5, pady=2)
+        # self.rotation_input = Entry(frame, width=10)
+        # self.rotation_input.grid(row=5, column=1, padx=5, pady=2)
+
+        # btn_rotation = Button(frame, text="Rotation", command=self.rotation)
+        # btn_rotation.grid(row=5, column=2, padx=5, pady=2)
 
         # Linha do botão "To Origin"
-        btn_to_origin = Button(frame, text="To Origin", command=self.to_origin)
-        btn_to_origin.grid(row=6, column=0, columnspan=3, pady=10)
-
+        #btn_to_origin = Button(frame, text="To Origin", command=self.to_origin)
+        #btn_to_origin.grid(row=6, column=0, columnspan=3, pady=10)
 
     def translate(self):
-        x = float(self.x_input.get())
-        y = float(self.y_input.get())
-        z = float(self.z_input.get())
-
+        data = self.translate_frame.get_input()
         shape = self.gl_window.get_selected()
-        shape.translate(x, y, z)
+        shape.translate(
+            x=data[0],
+            y=data[1],
+            z=data[2]
+        )
 
     def to_origin(self):
         shape = self.gl_window.get_selected()
@@ -70,6 +59,6 @@ class EditShape(Frame):
 
     def change_color(self):
         pass
-    
+
     def test():
         pass
